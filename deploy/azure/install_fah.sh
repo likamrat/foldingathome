d33917aef2316ee515ac885776a6d6412967e1e6
likamrat <likamrat@microsoft.com>
2020-03-26 11:09:36 -0700

    azure deploy

--- deploy/azure/install_fah.sh
+++ deploy/azure/install_fah.sh
@@ -2,67 +2,6 @@
 
 sudo apt-get update
 
-sudo cat <<EOT >> config.xml
-<config>
-  <!-- User Information -->
-  <user value="Anonymous"/> <!-- Enter your user name here -->
-  <team value="999"/>         <!-- Your team number -->
-  <passkey value=""/>       <!-- 32 hexadecimal characters if provided -->
-
-  <power value="full"/>
-  <gpu value="false"/>      <!-- If true, attempt to autoconfigure GPUs -->
-  <fold-anon value="false"/>
-
-  <!-- Folding Slots
-    No folding slot configuration is necessary.  The client will
-    automaticlaly choose a good configuration for you.  However, here
-    are some examples:
-  
-      <slot id="0" type="CPU"/>
-
-    or
-    
-      <slot id="0" type="CPU"/>
-      <slot id="1" type="GPU"/>
-
-    All slots in a configuration MUST have unique ids.
-   -->
-
-  Granting Remote FAHControl Access
-    To grant remote access to FAHControl and the remote command server add
-    the following:
-
-       <allow v='127.0.0.1 0/0'/>
-      <!-- <password>PASS</password> -->
-
-    Where x.x.x.x is the IP address of the computer which should be granted
-    remote access and PASS is you choose to further secure the connection.
- 
-
-  Granting Remote Web Access
-    If you would like to grant remote Web access then you need these two
-    configuration lines:
-
-    <!-- HTTP Server -->
-
-    <allow v='127.0.0.1 0/0'/>
-
-    <!-- Web Server -->
-
-    <web-allow v='127.0.0.1 0/0'/>
-
-    Where x.x.x.x is the IP address of the computer which should be granted
-    remote access.  You may also need to open port 7396 on your firewall.
-
-    You can then access the Web interface remotely by going to this address:
-
-      http://y.y.y.y:7396/
-
-    Where y.y.y.y is the IP address of this computer.
- 
-</config>
-EOT
-
 export VERSION="v7.5"
 
 sudo mkdir -p /opt/fahclient && \
@@ -79,7 +18,7 @@ sudo apt-get clean autoclean && \
 sudo apt-get autoremove --yes && \
 sudo rm -rf /var/lib/apt/lists/*
 
+curl -l https://github.com/likamrat/foldingathome/blob/master/deploy/azure/config.xml -o config.xml
 sudo cp config.xml /opt/fahclient && /
     cd /opt/fahclient && /
     ./FAHClient
-
