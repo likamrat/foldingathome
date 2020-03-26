cac5d640c375e97312bccd84c90342fff6b19e5a
likamrat <likamrat@microsoft.com>
2020-03-25 19:49:38 -0700

    initial commit

---
+++ deploy/docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+/opt/fahclient/FAHClient \
+    --user="${USER}" \
+    --team="${TEAM}" \
+    --passkey="${PASSKEY}" \
+    --gpu="${ENABLE_GPU}" \
+    --smp="${ENABLE_SMP}" \
+    --power=full \
+    --gui-enabled=false \
+
+exec "$@"
