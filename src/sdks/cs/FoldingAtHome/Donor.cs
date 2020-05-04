e90e98f8f394abea1cc0f3ed9bf0bbf45c3bde63
Adam Grocholski <adgroc@microsoft.com>
2020-05-04 17:52:23 -0500

    donor class

---
+++ src/sdks/cs/FoldingAtHome/Donor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FoldingAtHome
+{
+    public class Donor
+    {
+        [JsonPropertyName("wus")]
+        public int WorkUnits { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("rank")]
+        public int? Rank { get; set; }
+        [JsonPropertyName("credit")]
+        public int Credit { get; set; }
+        [JsonPropertyName("team")]
+        public int Team { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+    }
+}
