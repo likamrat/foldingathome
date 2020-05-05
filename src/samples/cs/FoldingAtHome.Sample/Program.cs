39e1e4d8b9edd889426df6dd85b4f2fb87c87512
Adam Grocholski <adgroc@microsoft.com>
2020-05-05 14:25:13 -0500

    samples

---
+++ src/samples/cs/FoldingAtHome.Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FoldingAtHome;
+
+namespace FoldingAtHome.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var team = Team.Get(264470).Result;
+
+            Console.WriteLine(team);
+
+            foreach (var donor in team.Donors)
+                Console.WriteLine("--{0}", donor);
+        }
+    }
+}
