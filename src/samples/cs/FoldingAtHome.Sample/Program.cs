01648adaaf11880dc9d33df15e289057d945100f
Adam Grocholski <adgroc@microsoft.com>
2020-05-06 13:35:48 -0500

    updated sample app to show donor api

--- src/samples/cs/FoldingAtHome.Sample/Program.cs
+++ src/samples/cs/FoldingAtHome.Sample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using FoldingAtHome;
 
 namespace FoldingAtHome.Sample
@@ -9,10 +10,20 @@ namespace FoldingAtHome.Sample
         {
             var team = Team.Get(264470).Result;
 
+            Console.WriteLine("TEAM INFORMATION");
             Console.WriteLine(team);
 
-            foreach (var donor in team.Donors)
-                Console.WriteLine("-{0}", donor);
+            foreach (var teamDonors in team.Donors)
+                Console.WriteLine("-{0}", teamDonors);
+
+            Console.WriteLine("");
+            Console.WriteLine("DONOR INFORMATION");
+
+            var donor = Donor.Get("adgroc").Result;
+
+            Console.WriteLine("name: {0}", donor.Name);
+            Console.WriteLine("work units: {0}", donor.WorkUnits);
+            Console.WriteLine("credit: {0}", donor.Credit);
         }
     }
 }
