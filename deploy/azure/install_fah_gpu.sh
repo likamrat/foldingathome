7254657775380dbc7bc243f830eac6c8b07ce6ba
Chris Mitchell (OCP) <chmitch@microsoft.com>
2020-05-13 12:04:34 -0500

    add cuda install

--- deploy/azure/install_fah_gpu.sh
+++ deploy/azure/install_fah_gpu.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+#download and install Nvidia GPU Drivers
+CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
+rm -f /tmp/${CUDA_REPO_PKG}
 sudo apt-get update
+sudo apt-get install cuda-drivers
+
 
 export VERSION="v7.6"
 
