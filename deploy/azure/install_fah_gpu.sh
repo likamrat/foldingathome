348eb01666e4f3015bafc907340f597df91211da
Chris Mitchell (OCP) <chmitch@microsoft.com>
2020-05-14 10:04:05 -0500

    why is this config file not getting populated correctly

--- deploy/azure/install_fah_gpu.sh
+++ deploy/azure/install_fah_gpu.sh
@@ -24,54 +24,19 @@ sudo rm -rf /var/lib/apt/lists/*
 
 sudo cat <<EOT >> /opt/fahclient/config.xml
 <config>
-  <!-- User Information -->
-  <user value="Anonymous"/> <!-- Enter your user name here -->
-  <team value="999"/>         <!-- Your team number -->
-  <passkey value=""/>       <!-- 32 hexadecimal characters if provided -->
+  <user value="Anonymous"/>
+  <team value="999"/>      
+  <passkey value=""/>      
 
   <power value="full"/>
-  <gpu value="true"/>      <!-- If true, attempt to autoconfigure GPUs -->
+  <gpu value="true"/>
   <fold-anon value="false"/>
 
-  <!-- Folding Slots
-    No folding slot configuration is necessary.  The client will
-    automaticlaly choose a good configuration for you.  However, here
-    are some examples:
-
-      <slot id="0" type="CPU"/>
-
-    or
-
-      <slot id="0" type="CPU"/>
-      <slot id="-1" type="GPU"/>
-
-    All slots in a configuration MUST have unique ids.
-   -->
-
-  <!-- Granting Remote FAHControl Access
-    To grant remote access to FAHControl and the remote command server add
-    the following: -->
-
-       <allow v='127.0.0.1 0/0'/>
-       <web-allow v='127.0.0.1 0/0'/>
-  <!-- <password>PASS</password> -->
-
-    <!-- Where x.x.x.x is the IP address of the computer which should be granted
-    remote access and PASS is you choose to further secure the connection.
-
-
-  Granting Remote Web Access
-    If you would like to grant remote Web access then you need these two
-    configuration lines: -->
-
-
-    <!-- HTTP Server -->
-    <!-- Web Server -->
-
-    <slot id="0" type="CPU"/>
-    <slot id="1" type="GPU"/>
-
-    
+  <allow v='127.0.0.1 0/0'/>
+  <web-allow v='127.0.0.1 0/0'/>
+  
+  <slot id="0" type="CPU"/>
+  <slot id="1" type="GPU"/>  
 </config>
 EOT
 
@@ -84,4 +49,4 @@ EOT
 
 sudo chmod +x run_fah.sh
 
-at now + 1 minutes -f run_fah.sh
+at now + 3 minutes -f run_fah.sh
